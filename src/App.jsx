cf7c7ed11103c3ff411c81c04971284f62de55b7
vansharorx <vansharora21102005@gmail.com>
2026-05-31 06:45:11 +0530

    feat: initialize React frontend with Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[#F7E7CE]">
+      <div className="text-center">
+
+        <h1 className="heading-font text-5xl font-bold text-[#102C26]">
+          DevFlow
+        </h1>
+
+        <p className="mt-4 text-gray-700">
+          Enterprise Issue Tracking Platform
+        </p>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
